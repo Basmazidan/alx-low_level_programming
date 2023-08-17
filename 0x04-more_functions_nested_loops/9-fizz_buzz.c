710a902f91cfde23a36dfcb49563e02046c04bcb
basmazidan <basmazidan041@outlook.com>
2023-08-17 16:14:37 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,10 +18,10 @@ int main(void)
 			printf(" Fizz ");
 		} else if (i % 5 == 0 && i % 5 != 0)
 		{
-			printf(" Buzz ");
+			printf("Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz ");
+			printf("FizzBuzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
