6829ce0ebb33884ad4adce3c540cf732209354cf
Basmazidan <Basmazidan041@outlook.con>
2023-08-21 22:04:06 +0000

    All Done

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -7,7 +8,7 @@
  * Return: on success 1.
  * on error, -1 is returned, and errno is set appropriately
  */
-int_putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
